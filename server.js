6b4963b58a9f91f084b7b49c11f1c90379ceb68f
minmoong <62737839+minmoong@users.noreply.github.com>
2022-05-06 19:56:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://votemotion.kr"],
     methods: ["GET", "POST"],
     credentials: true
   })
